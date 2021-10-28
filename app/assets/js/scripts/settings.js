78c51f501a1b2c782e5f16a62982913327d1b45b
ByMoniXX <olis020926@gmail.com>
2021-10-28 13:01:09 -0500

    Fix
    Fix launcher

--- app/assets/js/scripts/settings.js
+++ app/assets/js/scripts/settings.js
@@ -1305,7 +1305,7 @@ function populateSettingsUpdateInformation(data){
         settingsUpdateTitle.innerHTML = 'Estas en la version mas reciente'
         settingsUpdateChangelogCont.style.display = 'none'
         populateVersionInformation(remote.app.getVersion(), settingsUpdateVersionValue, settingsUpdateVersionTitle, settingsUpdateVersionCheck)
-        settingsUpdateButtonStatus('Revisar si hay actualizaciones', true, () => {
+        settingsUpdateButtonStatus('Revisar si hay actualizaciones', false, () => {
             if(!isDev){
                 ipcRenderer.send('autoUpdateAction', 'checkForUpdate')
                 settingsUpdateButtonStatus('Revisando si hay actualizaciones...', true)
