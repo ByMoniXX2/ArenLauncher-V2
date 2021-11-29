55d90cdb81b53853c959f9da78e5b4538891d285
ByMoniXX <olis020926@gmail.com>
2021-11-29 11:03:11 -0500

    Add DiscordWrapper

--- app/assets/js/scripts/welcome.js
+++ app/assets/js/scripts/welcome.js
@@ -1,6 +1,10 @@
 /**
  * Script for welcome.ejs
  */
-document.getElementById('welcomeButton').addEventListener('click', e => {
+ document.getElementById('welcomeButton').addEventListener('click', e => {
     switchView(VIEWS.welcome, VIEWS.login)
+    if(hasRPC){
+        DiscordWrapper.updateDetails('Adding an Account...')
+        DiscordWrapper.updateState('Launcher Setup')
+    }
 })
