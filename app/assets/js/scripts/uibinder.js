57e7b26b26fbe26a039c35242058aae3f1c47396
ByMoniXX <olis020926@gmail.com>
2021-11-29 11:12:00 -0500

    Fail Back

--- app/assets/js/scripts/uibinder.js
+++ app/assets/js/scripts/uibinder.js
@@ -80,33 +80,24 @@ function showMainUI(data){
 
         if(ConfigManager.isFirstLaunch()){
             currentView = VIEWS.welcome
-            $(VIEWS.welcome).fadeIn(100)
-            if(hasRPC){
-                DiscordWrapper.updateDetails('En pantalla de bienvenida')
-                DiscordWrapper.updateState('Setup del Launcher')
-            }
+            $(VIEWS.welcome).fadeIn(1000)
         } else {
             if(isLoggedIn){
                 currentView = VIEWS.landing
-                $(VIEWS.landing).fadeIn(100)
-                if(hasRPC && !ConfigManager.isFirstLaunch()){
-                    if(ConfigManager.getSelectedServer()){
-                        const serv = DistroManager.getDistribution().getServer(ConfigManager.getSelectedServer())
-                        DiscordWrapper.updateDetails('Listo para jugar!')
-                    } else {
-                        DiscordWrapper.updateDetails('En el menu...')
-                    }
-                }
+                $(VIEWS.landing).fadeIn(1000)
             } else {
                 currentView = VIEWS.login
-                $(VIEWS.login).fadeIn(100)
-                if(hasRPC){
-                    DiscordWrapper.updateDetails('Agregando una cuenta...')
-                    DiscordWrapper.clearState()
-                }
+                $(VIEWS.login).fadeIn(1000)
             }
         }
-    }, 250)
+
+        setTimeout(() => {
+            $('#loadingContainer').fadeOut(500, () => {
+                $('#loadSpinnerImage').removeClass('rotating')
+            })
+        }, 250)
+        
+    }, 750)
     // Disable tabbing to the news container.
     initNews().then(() => {
         $('#newsContainer *').attr('tabindex', '-1')
